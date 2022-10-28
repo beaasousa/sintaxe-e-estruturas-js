f57b8cdea55b72aa7729a40b67cb706cae011f1c
Ana Beatriz Costa de Sousa <anacostasousa08@gmail.com>
2022-10-27 22:17:41 -0300

    finaliza

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('hello world'); //nao declarar a atribuiçao n da errro mas n eh recomendavel
+
+let mensagem = 'ola mundo'; //variavel pode alterar
+console.log(mensagem);
+
+const despedida = 'bye world'; //constante n pode alterar
+console.log(despedida);
+
+// var blablabla = 'blablabla' (nao usar !!!)
+
+nome = 'Ana';
+sobrenome = 'Sousa';
+nomeCompleto = `${nome} ${sobrenome}`;
+
+// nome = null (apaga o nome)
+
+let email;
+
+email ?? 'nao infomardo';
